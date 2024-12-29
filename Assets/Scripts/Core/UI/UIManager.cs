dbfd1961ff6bd249b908256776a950e9693b2239
Jeffrey Gaitan <86032349+jeffreyg8940@users.noreply.github.com>
2024-12-29 18:11:58 -0500

    Implementing Pause Screen
    Pause Screen Implemented

--- Assets/Scripts/Core/UI/UIManager.cs
+++ Assets/Scripts/Core/UI/UIManager.cs
@@ -4,9 +4,14 @@ using UnityEngine.SceneManagement;
 
 
 public class UIManager : MonoBehaviour
-{
+{   
+    [Header("GameOver")]
     [SerializeField] private GameObject gameOverScreen;
     [SerializeField] private GameObject HealthBar;
+
+    [Header("Pause")]
+    [SerializeField] private GameObject pauseScreen;
+    
     // private AudioClip gameOverSound;
 
     private void Awake()
@@ -14,8 +19,10 @@ public class UIManager : MonoBehaviour
         // just in case im dumb I will deactivate the gamejover screen lol also will activate healthbar too
         HealthBar.SetActive(true);
         gameOverScreen.SetActive(false); 
+        pauseScreen.SetActive(false);
 
     }
+    #region Game Over
 
     public void GameOver()
     {
@@ -38,4 +45,41 @@ public class UIManager : MonoBehaviour
     {
         Application.Quit();
     }
+    #endregion
+
+    #region Pause
+    public void PauseGame(bool status)
+    {
+        pauseScreen.SetActive(status);
+        HealthBar.SetActive(!status);
+
+        if(status)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            // if the pause screen is already active, make it UNACTIVE!! 
+            if(pauseScreen.activeInHierarchy)
+                PauseGame(false);
+            else
+                PauseGame(true);
+        }
+    }
+
+        public void pauseRestart()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    #endregion
 }
---
+++ Assets/Scripts/Player Scripts/PlayerMelee.cs
@@ -0,0 +1,77 @@
+using Unity.Mathematics;
+using Unity.VisualScripting;
+using UnityEditor.Search;
+using UnityEngine;
+
+public class PlayerMelee : MonoBehaviour
+{
+    // setting up variables
+    [Header("Attack Params")]
+    [SerializeField] private float attackCoolDown;
+    [SerializeField] private float range;
+    [Header("collider Params")]
+    [SerializeField] private float colliderDistance;
+    [SerializeField] private float damage;
+    [SerializeField] private BoxCollider2D boxCollider;
+    [SerializeField] private LayerMask playerLayer;
+
+    private float cooldownTimer = Mathf.Infinity;
+
+    private Animator anim; 
+
+    private Health playerHealth;
+
+    private EnemyPatrol enemyPatrol;
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        enemyPatrol = GetComponentInParent<EnemyPatrol>();
+    }
+
+    private void Update()
+    {
+        cooldownTimer += Time.deltaTime;
+        //Attack player only when seen
+
+        if(playerInsight())
+        {
+            if(cooldownTimer >= attackCoolDown)
+            {
+                cooldownTimer = 0;
+                anim.SetTrigger("meeleeattack");
+            }
+        }
+
+        if(enemyPatrol != null)
+        {
+            enemyPatrol.enabled = !playerInsight();
+        }
+
+    }
+
+    private bool playerInsight()
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance, 
+        new Vector3 (boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z), 0, Vector2.left, 0, playerLayer);
+
+        if(hit.collider != null)
+        {
+            playerHealth = hit.transform.GetComponent<Health>();   
+        }
+        return hit.collider != null;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance, new Vector3 (boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z));
+    }
+
+    private void damagePlayer()
+    {
+        if(playerInsight())
+        {
+            playerHealth.takeDamage(damage);
+        }
+    }
+}
